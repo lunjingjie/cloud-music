212c4eb61eb20c9a3a49b2c7e29f812a29c61179
lunjingjie <lunjj@create80.com>
2022-09-08 16:53:06 +0800

    fix: swiper

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,42 +1,54 @@
 import React, { useEffect, useState } from 'react';
-import Swiper from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
 import { SliderContainer } from '../../components/slider/style';
 
+// SwiperCore.use([Pagination, Autoplay]);
+
 function Slider(props) {
   const { bannerList } = props;
-  const [sliderSwiper, setSliderSwiper] = useState(null);
+  // const [sliderSwiper, setSliderSwiper] = useState(null);
 
-  useEffect(() => {
-    if (bannerList.length > 0 && !sliderSwiper) {
-      let newSliderSwiper = new Swiper('slider-container', {
-        loop: true,
-        autoplay: {
-          delay: 3000,
-          disableOnInteraction: false,
-        },
-        pagination: {el:'.swiper-pagination'},
-      });
-      setSliderSwiper(newSliderSwiper);
-    }
-  }, [bannerList.length, sliderSwiper]);
+  // useEffect(() => {
+  //   if (bannerList.length > 0 && !sliderSwiper) {
+  //     let newSliderSwiper = new Swiper('slider-container', {
+  //       loop: true,
+  //       autoplay: {
+  //         delay: 3000,
+  //         disableOnInteraction: false,
+  //       },
+  //       pagination: {el:'.swiper-pagination'},
+  //     });
+  //     setSliderSwiper(newSliderSwiper);
+  //   }
+  // }, [bannerList.length, sliderSwiper]);
 
   return (
     <SliderContainer>
       <div className='before'></div>
       <div className="slider-container">
         <div className="swiper-wrapper">
-          {
-            bannerList.map (slider => {
-              return (
-                <div className="swiper-slide" key={slider.imageUrl}>
-                  <div className="slider-nav">
+          <Swiper
+            modules={[]}
+            spaceBetween={10}
+            slidesPerView={1}
+            loop
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            pagination={{ el: '.swiper-pagination' }}
+          >
+            {
+              bannerList.map (slider => {
+                return (
+                  <SwiperSlide key={slider.imageUrl}>
                     <img src={slider.imageUrl} width="100%" height="100%" alt="推荐" />
-                  </div>
-                </div>
-              );
-            })
-          }
+                  </SwiperSlide>
+                );
+              })
+            }
+          </Swiper>
         </div>
         <div className="swiper-pagination"></div>
       </div> 
