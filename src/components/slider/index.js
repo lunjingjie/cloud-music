5643ae7a37d039c79266142ee08ec2d258bfed87
lunjingjie <lunjj@create80.com>
2022-09-15 11:52:24 +0800

    fix: swiper v8.x

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,57 +1,42 @@
 import React, { useEffect, useState } from 'react';
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper.min.css";
+import Swiper from 'swiper/swiper-bundle.esm';
+import "swiper/swiper-bundle.min.css";
 import { SliderContainer } from '../../components/slider/style';
 
 // SwiperCore.use([Pagination, Autoplay]);
 
 function Slider(props) {
   const { bannerList } = props;
-  // const [sliderSwiper, setSliderSwiper] = useState(null);
+  const [sliderSwiper, setSliderSwiper] = useState(null);
 
-  // useEffect(() => {
-  //   if (bannerList.length > 0 && !sliderSwiper) {
-  //     let newSliderSwiper = new Swiper('slider-container', {
-  //       loop: true,
-  //       autoplay: {
-  //         delay: 3000,
-  //         disableOnInteraction: false,
-  //       },
-  //       pagination: {el:'.swiper-pagination'},
-  //     });
-  //     setSliderSwiper(newSliderSwiper);
-  //   }
-  // }, [bannerList.length, sliderSwiper]);
+  useEffect(() => {
+    if (bannerList.length > 0 && !sliderSwiper) {
+      let newSliderSwiper = new Swiper('.swiper', {
+        autoplay: true,
+        loop: true,
+        pagination: {el:'.swiper-pagination'},
+      });
+      setSliderSwiper(newSliderSwiper);
+    }
+  }, [bannerList.length, sliderSwiper]);
 
   return (
     <SliderContainer>
       <div className='before'></div>
-      <div className="slider-container">
+      <div className="swiper">
         <div className="swiper-wrapper">
-          <Swiper
-            modules={[]}
-            spaceBetween={10}
-            slidesPerView={1}
-            loop
-            autoplay={{
-              delay: 3000,
-              disableOnInteraction: false,
-            }}
-            pagination={{ el: '.swiper-pagination' }}
-          >
             {
-              bannerList.map (slider => {
+              bannerList.map(slider => {
                 return (
-                  <SwiperSlide key={slider.imageUrl}>
+                  <div className="swiper-slide" key={slider.imageUrl}>
                     <img src={slider.imageUrl} width="100%" height="100%" alt="推荐" />
-                  </SwiperSlide>
+                  </div>
                 );
               })
             }
-          </Swiper>
         </div>
         <div className="swiper-pagination"></div>
-      </div> 
+    </div>
     </SliderContainer>
   );
 }
--- src/components/slider/style.js
+++ src/components/slider/style.js
@@ -15,19 +15,13 @@ export const SliderContainer = styled.div`
     height: 60%;
     background-color: ${style['theme-color']};
   }
-  .slider-container {
+  .swiper {
     position: relative;
     width: 98%;
     height: 160px;
     overflow: hidden;
     margin: auto;
     border-radius: 6px;
-    .slider-nav {
-      position: absolute;
-      display: block;
-      width: 100%;
-      height: 100%;
-    }
     .swiper-pagination-bullet-active {
       background: ${style["theme-color"]};
     }
